e1a088f742a0f4c696c5361ca6dd6f4df46bce77
Hanks Huang <hankshuangs@gmail.com>
2017-12-14 00:15:08 +0800

    state

---
+++ Models/iAAA/MvcDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Models.iAAA
+{
+    public class MvcDbContext : DbContext
+    {
+        public MvcDbContext() : base("name=MvcDbContext")
+        {
+        }
+
+        //重要對應資料表
+        private DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); //資料表名不要自動變複數
+        }
+    }
+}
---
+++ Models/iAAA/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models.iAAA
+{
+    internal class User
+    {
+        [Key]
+        [Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //自動編號
+        [DisplayName("學號")]
+        public int ID { get; set; }
+
+        [DisplayName("姓名")]
+        [MaxLength(5)]
+        [Column(TypeName = "nvarchar")]
+        [Required] //不為空
+        public string Name { get; set; }
+
+        [DisplayName("生日")]
+        public DateTime Birthday { get; set; }
+
+        //浮點數
+        [DisplayName("視力")]
+        public decimal Eye { get; set; }
+    }
+}
